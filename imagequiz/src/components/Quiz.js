0a4f2a0dde149bfe1bd52b197e532aaeffe13be3
Zachary Halperin <zacharykhalperin@gmail.com>
2022-03-25 19:59:37 -0700

    t

--- imagequiz/src/components/Quiz.js
+++ imagequiz/src/components/Quiz.js
@@ -9,12 +9,11 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Spinner from "react-bootstrap/Spinner";
 
 const Quiz = () => {
-    const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+    let [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
     const [quiz, setQuiz] = useState(undefined);
-    const [score, setScore] = useState(0);
+    let [score, setScore] = useState(0);
     const [answer, setAnswer] = useState("");
     const { id } = useParams();
-
 
     useEffect(() => {
         if (!quiz) {
@@ -22,6 +21,7 @@ const Quiz = () => {
             setQuiz(x);
             console.log(x);
         }
+        
     });
 
     return (
@@ -32,18 +32,38 @@ const Quiz = () => {
                         <Card className='h-100'>
                             <Card.Img variant="top" src={quiz.questions[currentQuestionNumber].picture}></Card.Img>
                             <Card.Body>
-                                <Card.Title align="center">{quiz.name}</Card.Title>
+                                <Card.Title align="center">{quiz.questions[currentQuestionNumber].answer}</Card.Title>
                                 <Card.Title>
                                     Let's take the quiz now!
                                 </Card.Title>
                             </Card.Body>
                             <ListGroup>
                                 {quiz.questions[currentQuestionNumber].choices.map(x =>
-                                    <ListGroup.Item onClick={() => setAnswer(x)}>{x}</ListGroup.Item>
+                                    <ListGroup.Item onClick={() => {
+                                        console.log(x);
+                                        setAnswer(x);
+                                        if(x == quiz.questions[currentQuestionNumber].answer){
+                                            
+                                            console.log('Correct');
+                                            setScore(score+=1);
+
+                                        }else{
+                                            console.log('Incorrect');
+                                        }
+                                        if(currentQuestionNumber < quiz.questions.length-1){
+                                            setCurrentQuestionNumber(currentQuestionNumber+=1);
+                                        }else{
+                                            setCurrentQuestionNumber(currentQuestionNumber=0);
+                                        }
+                                        console.log(currentQuestionNumber);
+                                        console.log(score);
+                                    }
+                                    }>{x}</ListGroup.Item>
                                 )}
                             </ListGroup>
                             <Card.Title>U clicked {answer}</Card.Title>
                             <Card.Title>Answer is {quiz.questions[currentQuestionNumber].answer}</Card.Title>
+                            
                             <Card.Title>Ur score: {score}</Card.Title>
                         </Card>
                         :
